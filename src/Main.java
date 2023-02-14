feadaf6ca1978aee447330c597cac504b127a8e3
Oksana Volkova <xeniavolchica@gmail.com>
2023-02-14 21:28:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 370;// средства на счете
+        int y = 1700; // пополнение счета
+        int t = y / 100; // бонусы
+
+        if (y > 1000) {
+            System.out.println("На вашем счете:" + (x + y + t) + "рублей");
+            System.out.println("В том числе начислено бонусов:" + t + "рублей");
+        } else {
+            System.out.println("На вашем счете:" + (x + y) + "рублей");
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
